73fdeade06d40edc2bec2ed0f9ece4bfc4862172
kavithanuranga1-cyber <kavithanuranga1@gmail.com>
2026-07-23 17:46:08 +0530

    Add files via upload

--- papers-data.js
+++ papers-data.js
@@ -1,4 +1,7 @@
-/* Add real PDF links here later. Example:
-window.PAPER_LINKS['grade1-5|Grade 3|Mathematics|2025|Term 1|Sinhala'] = 'pdfs/grade3-maths-2025-t1-si.pdf';
-*/
-window.PAPER_LINKS = {};
+/* Kavi Past Papers PDF links */
+window.PAPER_LINKS = {
+  'scholarship|Past Papers|Scholarship Paper|2025|Sinhala|Paper 1': 'pdfs/scholarship/2025/scholarship-2025-sinhala-paper-1.pdf',
+  'scholarship|Past Papers|Scholarship Paper|2025|Sinhala|Paper 2': 'pdfs/scholarship/2025/scholarship-2025-sinhala-paper-2.pdf',
+  'scholarship|Past Papers|Scholarship Paper|2025|Tamil|Paper 1': 'pdfs/scholarship/2025/scholarship-2025-tamil-paper-1.pdf',
+  'scholarship|Past Papers|Scholarship Paper|2025|Tamil|Paper 2': 'pdfs/scholarship/2025/scholarship-2025-tamil-paper-2.pdf'
+};
