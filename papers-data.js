781c45364e42e0e4adebb0ecf44ecd3efa5c838f
kavithanuranga1-cyber <kavithanuranga1@gmail.com>
2026-07-24 16:33:54 +0530

    Update papers-data.js

--- papers-data.js
+++ papers-data.js
@@ -1,11 +1,13 @@
 /* Kavi Past Papers PDF links */
 window.PAPER_LINKS = {
-  'scholarship|2025|Sinhala|Paper 1': 'pdfs/scholarship/2025/scholarship-2025-sinhala-paper-1.pdf',
-  'scholarship|2025|Sinhala|Paper 2': 'pdfs/scholarship/2025/scholarship-2025-sinhala-paper-2.pdf',
-  'scholarship|2025|Tamil|Paper 1': 'pdfs/scholarship/2025/scholarship-2025-tamil-paper-1.pdf',
-  'scholarship|2025|Tamil|Paper 2': 'pdfs/scholarship/2025/scholarship-2025-tamil-paper-2.pdf'
-};
+  "scholarship|2025|Sinhala|Paper 1": "pdfs/scholarship/2025/scholarship-2025-sinhala-paper-1.pdf",
+  "scholarship|2025|Sinhala|Paper 2": "pdfs/scholarship/2025/scholarship-2025-sinhala-paper-2.pdf",
+  "scholarship|2025|Tamil|Paper 1": "pdfs/scholarship/2025/scholarship-2025-tamil-paper-1.pdf",
+  "scholarship|2025|Tamil|Paper 2": "pdfs/scholarship/2025/scholarship-2025-tamil-paper-2.pdf",
 
+  "ol|2022|Sinhala|Mathematics|Paper 1": "pdfs/ol/2022/2022_OL_Mathematics_Sinhala_Paper_1.pdf",
+  "ol|2022|Sinhala|Mathematics|Paper 2": "pdfs/ol/2022/2022_OL_Mathematics_Sinhala_Paper_2.pdf"
+};
 /* Add marking scheme links using the same lookup key when files are available. */
 window.MARKING_LINKS = {
 };
